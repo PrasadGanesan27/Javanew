08cb05f2189f7c3552311c2480190cbd3b19f87f
PrasadGanesan27 <147181840+PrasadGanesan27@users.noreply.github.com>
2023-10-09 13:03:51 +0530

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+	    int result =10 * 12;
+String answer ="the answe is" + result
+Systerm.outprintln(answer);
     }
 }
